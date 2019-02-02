013886cbfe9cf3907945aebc1ce99626e4215bbf
Mark Meretzky <mark.meretzky@gmail.com>
2019-02-01 22:37:07 -0500

    Commit.

--- ScrollingForm/ViewController.swift
+++ ScrollingForm/ViewController.swift
@@ -6,15 +6,52 @@
 //  Copyright © 2019 New York University School of Professional Studies. All rights reserved.
 //
 
-import UIKit
+import UIKit;
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!;   //contains 8 UIView's
+
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
+        registerForKeyboardNotifications();
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWasShown(_:)),
+            name: UIResponder.keyboardDidShowNotification,
+            object: nil);
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillBeHidden(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil);
+    }
+    
+    @objc func keyboardWasShown(_ notification: NSNotification) {
+        guard let info: [AnyHashable: Any] = notification.userInfo,
+            let keyboardFrameValue: NSValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else {
+            return;
+        }
+        
+        let height: CGFloat = keyboardFrameValue.cgRectValue.size.height;
+        let contentInsets: UIEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: height, right: 0.0);
+        scrollView.contentInset = contentInsets;
+        scrollView.scrollIndicatorInsets = contentInsets;
+    }
+    
+    @objc func keyboardWillBeHidden(_ notification: NSNotification) {
+        scrollView.contentInset = .zero;
+        scrollView.scrollIndicatorInsets = .zero;
     }
 
+    //Called when the return key of any UITextField is tapped.
 
+    @IBAction func returnKeyTapped(_ sender: UITextField) {
+        sender.resignFirstResponder();
+    }
 }
-
